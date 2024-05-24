41fe37e50b2e7ea4d93d0385e30914653429f84a
Katuki <katukifaith@gmail.com>
2024-05-24 04:59:31 +0300

    Add project files.

---
+++ HRLeaveManagement.Domain/LeaveType.cs
@@ -0,0 +1,14 @@
+﻿using HRLeaveManagement.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRLeaveManagement.Domain;
+
+public class LeaveType : BaseEntity
+{
+    public int DefaultDays { get; set; }
+    public string Name { get; set; } = string.Empty;
+}
